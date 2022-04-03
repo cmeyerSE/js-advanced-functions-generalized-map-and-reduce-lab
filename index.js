08bc8e3f11f7f4c8c84dc844cea8f2866296a904
Cory <cameyer182@gmail.com>
2022-04-02 21:35:47 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+function map(array, func) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(func(array[i]));
+    }
+    return newArray
+}
+
+function reduce(array, func, start) {
+    let s;
+
+    if(start) {
+        s = start;
+        for(let i = 0; i < array.length; i++) {
+            s = func(array[i], s)
+        }
+    }
+    else{
+        s = array[0];
+        for(let i = 1; i < array.length; i ++) {
+            s = func(array[i], s)
+        }
+    }
+    return s
+}
